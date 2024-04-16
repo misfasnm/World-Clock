176a61fdcbe8393931990c5f6f4620150f222bad
misfasnm <misfasnm@gmail.com>
2024-04-16 15:33:20 +0100

    Added current location

--- src/index.js
+++ src/index.js
@@ -28,6 +28,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
 
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 
